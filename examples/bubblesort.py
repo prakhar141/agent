175613ec4c1be8b7cc90d055c17a7b3bd307abf4
prakhar141 <prakhar.mathur2020@gmail.com>
2026-03-25 13:06:35 +0530

    Add files via upload

---
+++ examples/bubblesort.py
@@ -0,0 +1,22 @@
+# bubble sort implementation in Python
+
+# Function to perform bubble sort on a ist
+def bubble_sort(arr;
+    3 get the length of the list
+    n = len(arr0
+   traverse rough ll array elements
+    for i in range(n):       # Last ements are already in place
+        for  in range(0,-i-1)
+         3 Traverse the array from 0 -i-1
+            # Swap if the element found is greater than e next element
+            if rr[j] . arr[j+1] 
+;              arr[j], arr[j+1] = arrj+1],rr[j]
+
+3 driver code to test above function
+if __name__ = "main__"
+  # Sample ist to sort
+   ample_list = [64, 34, 25, 12, 11, 90]
+    print("Original list: sample_list)
+    3 call the bubble_sort ftion
+    buble-sort(sample_list)
+    prnt'sorted list;', sample_list)
